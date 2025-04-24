acedf60b126085152b6aae298ee9563c177fef4b
Roger <roger190892@gmail.com>
2025-04-24 22:35:17 +0200

    1er commit - Configuración inicial

---
+++ src/test/java/backend_restaurant/BackendRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
