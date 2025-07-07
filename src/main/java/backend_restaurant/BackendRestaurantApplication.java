3ba492c20527bbb580b9da07f844f7a7c7c783cf
Roger <roger190892@gmail.com>
2025-07-07 14:43:35 +0200

    Configurar nuevamente HikariCP. Configurar BackendRestaurantApplication para destruir conexiones activas cuando se cierra la app.

--- src/main/java/backend_restaurant/BackendRestaurantApplication.java
+++ src/main/java/backend_restaurant/BackendRestaurantApplication.java
@@ -1,13 +1,27 @@
 package backend_restaurant;
 
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class BackendRestaurantApplication {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendRestaurantApplication.class, args);
     }
 
+    @PreDestroy
+    public void close() throws Exception {
+        if (dataSource instanceof HikariDataSource hikariDataSource) {
+            hikariDataSource.close(); // Cierra conexiones al apagar el app
+        }
+    }
 }
