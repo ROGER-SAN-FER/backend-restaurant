fd66b853946067e14ce2f4356c671e7cc45ce651
Roger <roger190892@gmail.com>
2025-07-09 02:16:32 +0200

    Se comenta código en BackendRestaurantApplication ya que está insertado datos ya existentes en la BD creados en la primera ejecución

--- src/main/java/backend_restaurant/BackendRestaurantApplication.java
+++ src/main/java/backend_restaurant/BackendRestaurantApplication.java
@@ -34,64 +34,64 @@ public class BackendRestaurantApplication {
         }
     }
 
-    @Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            /* Create PERMISSIONS */
-            PermissionEntity createPermission = PermissionEntity.builder()
-                    .name("CREATE")
-                    .build();
-
-            PermissionEntity readPermission = PermissionEntity.builder()
-                    .name("READ")
-                    .build();
-
-            PermissionEntity updatePermission = PermissionEntity.builder()
-                    .name("UPDATE")
-                    .build();
-
-            PermissionEntity deletePermission = PermissionEntity.builder()
-                    .name("DELETE")
-                    .build();
-
-            PermissionEntity refactorPermission = PermissionEntity.builder()
-                    .name("REFACTOR")
-                    .build();
-
-            /* Create ROLES */
-            RoleEntity roleAdmin = RoleEntity.builder()
-                    .roleEnum(RoleEnum.ADMIN)
-                    .permissionsList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
-                    .build();
-
-            RoleEntity roleUser = RoleEntity.builder()
-                    .roleEnum(RoleEnum.USER)
-                    .permissionsList(Set.of(createPermission, readPermission, refactorPermission))
-                    .build();
-
-            RoleEntity roleInvited = RoleEntity.builder()
-                    .roleEnum(RoleEnum.INVITED)
-                    .permissionsList(Set.of(readPermission))
-                    .build();
-
-            RoleEntity roleDeveloper = RoleEntity.builder()
-                    .roleEnum(RoleEnum.DEVELOPER)
-                    .permissionsList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
-                    .build();
-
-            /* CREATE USERS */
-            UserEntity userRoger = UserEntity.builder()
-                    .username("admin")
-                    .password("$2a$10$zxz57PNKQetdgmh44vDBHuXNMj4YL7/Zd0NKu/288tynVBinL1FGm")
-                    .enabled(true)
-                    .accountNonExpired(true)
-                    .accountNonLocked(true)
-                    .credentialsNonExpired(true)
-                    .rolesList(Set.of(roleAdmin))
-                    .build();
-
-
-            userRepository.saveAll(List.of(userRoger));
-        };
-    }
+//    @Bean
+//    CommandLineRunner init(UserRepository userRepository) {
+//        return args -> {
+//            /* Create PERMISSIONS */
+//            PermissionEntity createPermission = PermissionEntity.builder()
+//                    .name("CREATE")
+//                    .build();
+//
+//            PermissionEntity readPermission = PermissionEntity.builder()
+//                    .name("READ")
+//                    .build();
+//
+//            PermissionEntity updatePermission = PermissionEntity.builder()
+//                    .name("UPDATE")
+//                    .build();
+//
+//            PermissionEntity deletePermission = PermissionEntity.builder()
+//                    .name("DELETE")
+//                    .build();
+//
+//            PermissionEntity refactorPermission = PermissionEntity.builder()
+//                    .name("REFACTOR")
+//                    .build();
+//
+//            /* Create ROLES */
+//            RoleEntity roleAdmin = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.ADMIN)
+//                    .permissionsList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
+//                    .build();
+//
+//            RoleEntity roleUser = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.USER)
+//                    .permissionsList(Set.of(createPermission, readPermission, refactorPermission))
+//                    .build();
+//
+//            RoleEntity roleInvited = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.INVITED)
+//                    .permissionsList(Set.of(readPermission))
+//                    .build();
+//
+//            RoleEntity roleDeveloper = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.DEVELOPER)
+//                    .permissionsList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
+//                    .build();
+//
+//            /* CREATE USERS */
+//            UserEntity userRoger = UserEntity.builder()
+//                    .username("admin")
+//                    .password("$2a$10$zxz57PNKQetdgmh44vDBHuXNMj4YL7/Zd0NKu/288tynVBinL1FGm")
+//                    .enabled(true)
+//                    .accountNonExpired(true)
+//                    .accountNonLocked(true)
+//                    .credentialsNonExpired(true)
+//                    .rolesList(Set.of(roleAdmin))
+//                    .build();
+//
+//
+//            userRepository.saveAll(List.of(userRoger));
+//        };
+//    }
 }
