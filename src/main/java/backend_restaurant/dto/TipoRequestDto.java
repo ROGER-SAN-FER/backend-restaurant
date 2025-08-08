9efc8a1fbcf0ab5c4d80b788e33d067da77683d0
Roger <roger190892@gmail.com>
2025-08-08 22:24:07 +0200

    Implementar DevTools en perfil dev

--- src/main/java/backend_restaurant/dto/TipoRequestDto.java
+++ src/main/java/backend_restaurant/dto/TipoRequestDto.java
@@ -1,9 +1,7 @@
 package backend_restaurant.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -12,6 +10,6 @@ public class TipoRequestDto {
 
     @NotBlank(message = "{message.nombre.NotBlank}")
     @Size(min = 4, max = 25, message = "{message.nombre.Size}")
-    @NotEmpty(message = "{message.nombre.NotNull}")
+    @NotEmpty(message = "{message.nombre.NotEmpty}")
     private String nombre;
 }
