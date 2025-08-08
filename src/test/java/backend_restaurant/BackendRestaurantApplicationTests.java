35ac44e5ad54107cd190515dc5eb40b7e3189aa4
Roger <roger190892@gmail.com>
2025-08-09 00:18:35 +0200

    Impementar perfil test en yml y validar test de BackendRestaurantApplication

--- src/test/java/backend_restaurant/BackendRestaurantApplicationTests.java
+++ src/test/java/backend_restaurant/BackendRestaurantApplicationTests.java
@@ -2,9 +2,11 @@ package backend_restaurant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class BackendRestaurantApplicationTests {
+@ActiveProfiles("test")
+    class BackendRestaurantApplicationTests {
 
     @Test
     void contextLoads() {
